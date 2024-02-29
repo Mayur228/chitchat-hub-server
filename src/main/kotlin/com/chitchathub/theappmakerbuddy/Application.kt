7ca8a0416b9728892f297b062f971b780b57cf40
mayur <mayursinh.parmar@knoxpo.com>
2024-02-29 12:02:18 +0530

    add config file

--- src/main/kotlin/com/chitchathub/theappmakerbuddy/Application.kt
+++ src/main/kotlin/com/chitchathub/theappmakerbuddy/Application.kt
@@ -5,11 +5,13 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(args: Array<String>) {
+    /*embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)*/
+    io.ktor.server.netty.EngineMain.main(args)
 }
 
+@Suppress("unused")
 fun Application.module() {
     configureSecurity()
     configureMonitoring()
