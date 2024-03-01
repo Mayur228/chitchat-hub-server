708402905d13ef7136dd9a67046d65cb2ad6ae8a
mayur <mayursinh.parmar@knoxpo.com>
2024-03-01 11:22:32 +0530

    remove mongo connection string

--- src/main/kotlin/com/chitchathub/theappmakerbuddy/mongodb/Mongodb.kt
+++ src/main/kotlin/com/chitchathub/theappmakerbuddy/mongodb/Mongodb.kt
@@ -5,7 +5,6 @@ import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import io.ktor.server.application.*
 
 fun Application.configureMongoDatabase(): MongoDatabase {
-//    val connectionString = "mongodb+srv://mayursinh228:Chino62987@cluster0.d2pt5pz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
     val client = MongoClient.create(connectionString = System.getenv("MONGO_URI"))
     return  client.getDatabase(databaseName = "chitchat_hub")
